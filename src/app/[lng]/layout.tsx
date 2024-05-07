1e5e1914c8740b2adf4dfce8a75d27e8e5dc0a8b
GSS Dev <luan.le@gss-sol.com>
2024-05-07 14:27:12 +0700

    First Commit

---
+++ src/app/[lng]/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+import { dir } from 'i18next'
+import { languages, fallbackLng } from '../i18n/settings'
+import { useTranslation } from '../i18n'
+
+export async function generateStaticParams() {
+  return languages.map((lng) => ({ lng }))
+}
+
+export async function generateMetadata({ params: { lng } }: {
+  params: {
+    lng: string;
+  };
+}) {
+  if (languages.indexOf(lng) < 0) lng = fallbackLng
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { t } = await useTranslation(lng)
+  return {
+    title: t('title'),
+    content: 'A playground to explore new Next.js 14 app directory features such as nested layouts, instant loading states, streaming, and component level data fetching.'
+  }
+}
+
+export default function RootLayout({
+  children,
+  params: {
+    lng
+  }
+}: Readonly<{
+  children: React.ReactNode;
+  params: {
+    lng: string;
+  };
+}>) {
+  return (
+    <html lang={lng} dir={dir(lng)}>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}

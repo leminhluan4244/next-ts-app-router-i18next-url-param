c114c17ef73e60dee2e862deeae371b9eee797b5
GSS Dev <luan.le@gss-sol.com>
2024-05-07 18:54:11 +0700

    Update file

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,16 @@ acceptLanguage.languages(languages)
 
 export const config = {
   // matcher: '/:lng*'
-  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)']
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|images).*)',
+  ]
 }
 
 export function middleware(req: NextRequest) {
